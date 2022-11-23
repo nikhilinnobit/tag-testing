f46c3164d9db5cbc4d60aa38182610e2642427a4
innobit <innobit@localhost.localdomain>
2022-11-23 11:51:33 +0530

    tag

--- script.sh
+++ script.sh
@@ -1,36 +1,73 @@
 #!/bin/bash
 
-CURTAG=`git describe --abbrev=0 --tags`;
-CURTAG="${CURTAG/v/}"
+# Increment a version string using Semantic Versioning (SemVer) terminology, and adds it to git-tags
+# - Customised from: https://github.com/fmahnke/shell-semver/blob/master/increment_version.sh)
 
-IFS='.' read -a vers <<< "$CURTAG"
+# INSTALL: put the script in a PATH directory. 'git' will recognize automatically the new command.
+# USAGE: "git tag++ -p"
+#        "git tag++ -Mp"
+# BEWARE: "git describe" returns the last-tag JUST for the current tree!
+#         if amend a commit the SHA will change and the eventual tag will still point to the old unchanged commit
+#         delete and recreate the tag, in order to fix it.
 
-MAJ=${vers[0]}
-MIN=${vers[1]}
-BUG=${vers[2]}
-echo "Current Tag: v$MAJ.$MIN.$BUG"
+# Parse command line options.
 
-for cmd in "$@"
+while getopts ":Mmp" Option
 do
-	case $cmd in
-		"--major")
-			# $((MAJ+1))
-			((MAJ+=1))
-			MIN=0
-			BUG=0
-			echo "Incrementing Major Version#"
-			;;
-		"--minor")
-			((MIN+=1))
-			BUG=0
-			echo "Incrementing Minor Version#"
-			;;
-		"--bug")
-			((BUG+=1))
-			echo "Incrementing Bug Version#"
-			;;
-	esac
+  case $Option in
+    M ) major=true;;
+    m ) minor=true;;
+    p ) patch=true;;
+  esac
 done
-NEWTAG="v$MAJ.$MIN.$BUG"
-echo "Adding Tag: $NEWTAG";
-git tag -a $NEWTAG -m $NEWTAG
+
+shift $(($OPTIND - 1))
+
+if [ $1 -z ]
+then
+  version=$(git describe --abbrev=0 --tags)
+else
+  version=$1
+fi
+
+# Build array from version string.
+
+a=( ${version//./ } )
+
+# If version string is missing or has the wrong number of members, show usage message.
+
+if [ ${#a[@]} -ne 3 ]
+then
+  echo "usage: $(basename $0) [-Mmp] major.minor.patch"
+  exit 1
+fi
+
+# Increment version numbers as requested.
+
+if [ ! -z $major ]
+then
+  ((a[0]++))
+  a[1]=0
+  a[2]=0
+fi
+
+if [ ! -z $minor ]
+then
+  ((a[1]++))
+  a[2]=0
+fi
+
+if [ ! -z $patch ]
+then
+  ((a[2]++))
+fi
+
+git tag "${a[0]}.${a[1]}.${a[2]}"
+
+if [ $? -eq 0 ]; then
+  echo "Added new tag: ${a[0]}.${a[1]}.${a[2]} (previous was $version)"
+  exit 0
+else
+  echo "Did you specify the target parts? (-Mmp) [ M=Major m=minor p=patch ]"
+  exit 1
+fi
