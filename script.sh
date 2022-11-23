cd4f32dbc7873c72af1de0d7a79b76db5943e141
innobit <innobit@localhost.localdomain>
2022-11-23 11:52:58 +0530

    new tag

--- script.sh
+++ script.sh
@@ -1,73 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
-# Increment a version string using Semantic Versioning (SemVer) terminology, and adds it to git-tags
-# - Customised from: https://github.com/fmahnke/shell-semver/blob/master/increment_version.sh)
+#Get the highest tag number
+VERSION=`git describe --abbrev=0 --tags`
+VERSION=${VERSION:-'0.0.0'}
 
-# INSTALL: put the script in a PATH directory. 'git' will recognize automatically the new command.
-# USAGE: "git tag++ -p"
-#        "git tag++ -Mp"
-# BEWARE: "git describe" returns the last-tag JUST for the current tree!
-#         if amend a commit the SHA will change and the eventual tag will still point to the old unchanged commit
-#         delete and recreate the tag, in order to fix it.
+#Get number parts
+MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+MINOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
 
-# Parse command line options.
+#Increase version
+PATCH=$((PATCH+1))
 
-while getopts ":Mmp" Option
-do
-  case $Option in
-    M ) major=true;;
-    m ) minor=true;;
-    p ) patch=true;;
-  esac
-done
+#Get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT`
 
-shift $(($OPTIND - 1))
+#Create new tag
+NEW_TAG="$MAJOR.$MINOR.$PATCH"
+echo "Updating to $NEW_TAG"
 
-if [ $1 -z ]
-then
-  version=$(git describe --abbrev=0 --tags)
+#Only tag if no tag already (would be better if the git describe command above could have a silent option)
+if [ -z "$NEEDS_TAG" ]; then
+    echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+    git tag $NEW_TAG
 else
-  version=$1
-fi
-
-# Build array from version string.
-
-a=( ${version//./ } )
-
-# If version string is missing or has the wrong number of members, show usage message.
-
-if [ ${#a[@]} -ne 3 ]
-then
-  echo "usage: $(basename $0) [-Mmp] major.minor.patch"
-  exit 1
-fi
-
-# Increment version numbers as requested.
-
-if [ ! -z $major ]
-then
-  ((a[0]++))
-  a[1]=0
-  a[2]=0
-fi
-
-if [ ! -z $minor ]
-then
-  ((a[1]++))
-  a[2]=0
-fi
-
-if [ ! -z $patch ]
-then
-  ((a[2]++))
-fi
-
-git tag "${a[0]}.${a[1]}.${a[2]}"
-
-if [ $? -eq 0 ]; then
-  echo "Added new tag: ${a[0]}.${a[1]}.${a[2]} (previous was $version)"
-  exit 0
-else
-  echo "Did you specify the target parts? (-Mmp) [ M=Major m=minor p=patch ]"
-  exit 1
+    echo "Already a tag on this commit"
 fi
